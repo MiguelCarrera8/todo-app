85d64d467f3fd436561ead75117a037ea8f4aa37
MiguelCarrera8 <miguecarrera65@gmail.com>
2023-06-26 17:22:16 +0200

    finalizada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-import { QRScanner } from '@ionic-native/qr-scanner/ngx';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { AlertController, IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicStorageModule } from '@ionic/storage';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,
+    IonicModule.forRoot(),
+    IonicStorageModule.forRoot(),
+    AppRoutingModule
+  ],
   providers: [
-    QRScanner,
+    BarcodeScanner,
+    AlertController,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,42 +1,183 @@
 import { Component } from '@angular/core';
-import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
-
+import { Storage } from '@ionic/storage';
+import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner/ngx';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  tarea = "";
+  tareas = [];
+  tareas_marcadas = [];
+
 
   constructor(
-    private qrScanner: QRScanner
+    private qrScanner: BarcodeScanner,
+    private storage: Storage,
+    private alertCtrl: AlertController
   ) { }
 
+  ngOnInit() {
+    this.storage.get('tarea').then(res => {
+      this.tareas = res;
+      console.log(this.tareas);
+
+    });
+    this.storage.get('tarea_marcada').then(res => {
+      this.tareas_marcadas = res;
+      console.log(this.tareas_marcadas);
+
+    })
+  }
+
   scanCode() {
-    this.qrScanner.prepare()
-      .then((status: QRScannerStatus) => {
-        if (status.authorized) {
-          // camera permission was granted
+    // console.log("Escaneando");
 
 
-          // start scanning
-          let scanSub = this.qrScanner.scan().subscribe((text: string) => {
-            console.log('Scanned something', text);
+    // this.qrScanner.scan().then((status) => {
+    //   console.log('Scanned', status);
+    //   let texto = status.text
+    //   // if (texto.startsWith("http")) {
+    //   //   this.inApp.create(texto, 'system');
+    //   // } else {
+    //   // }
+    //   console.log('Texto Escaneado', texto);
 
-            this.qrScanner.hide(); // hide camera preview
-            scanSub.unsubscribe(); // stop scanning
-          });
+    // }).catch((e) => {
+    //   console.log('Error', e);
+    //   // this.utilities.showToast('El Qr es incorrecto');
 
-        } else if (status.denied) {
-          // camera permission was permanently denied
-          // you must use QRScanner.openSettings() method to guide the user to the settings page
-          // then they can grant the permission from there
+    // })
+
+    this.storage.get('tarea').then(res => {
+
+      // res.splice(1, 1);
+      // this.storage.set('tarea', res);
+
+      let request = {
+        checked: false,
+        content: this.tarea,
+      }
+
+      // console.log(res);
+      if (res) {
+        res.push(request);
+        this.storage.set('tarea', res);
+        console.log(res);
+        this.tareas = res;
+        this.tarea = "";
+      } else {
+        this.storage.set('tarea', [request]);
+        this.tareas = [request];
+
+        console.log(res);
+        this.tarea = "";
+      }
+
+    })
+  }
+
+
+  marcar(index, value) {
+    console.log(index, value);
+    let tarea = this.tareas[index];
+    tarea.index = index;
+    tarea.checked = !value;
+    this.storage.get('tarea_marcada').then(res => {
+      console.log(res);
+
+      if (res == null) {
+        res = [tarea];
+      } else {
+        console.log(res);
+        res.splice(index, 0, tarea);
+      }
+      this.tareas_marcadas = res;
+      this.storage.set('tarea_marcada', res);
+      this.storage.get('tarea').then(res => {
+        if (res == null) {
+          res = [tarea];
         } else {
-          // permission was denied, but not permanently. You can ask for permission again at a later time.
+          console.log(res);
+          res.splice(index, 1); // Corrección aquí
         }
+        this.tareas = res;
+        this.storage.set('tarea', res);
       })
-      .catch((e: any) => console.log('Error is', e));
+    })
+  }
 
+  desmarcar(index, value) {
+    console.log(index, value);
+    let tarea = this.tareas_marcadas[index];
+    tarea.index = index;
+    tarea.checked = !value;
+    this.storage.get('tarea').then(res => {
+      if (res == null) {
+        res = [tarea];
+      } else {
+        console.log(res);
+        if (Array.isArray(res)) {
+          res.splice(index, 0, tarea);
+        } else {
+          res = [tarea];
+        }
+      }
+      this.tareas = res;
+      this.storage.set('tarea', res);
+      this.storage.get('tarea_marcada').then(res => {
+        if (res == null) {
+          res = [tarea];
+        } else {
+          console.log(res);
+          if (Array.isArray(res)) {
+            res.splice(index, 1);
+          } else {
+            res = [];
+          }
+        }
+        this.tareas_marcadas = res;
+        this.storage.set('tarea_marcada', res);
+      })
+    })
+  }
+  async delete(index, array) {
+    console.log(index);
+    const alert = await this.alertCtrl.create({
+      header: 'Borrar elemento',
+      message: '¿Estás seguro de borrar este elemento?',
+      mode: 'ios',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+          }
+        }, {
+          text: 'Aceptar',
+          handler: () => {
+            if (array == 1) {
+              this.storage.get('tarea').then(res => {
+                res.splice(index, 1);
+
+                this.tareas = res;
+                this.storage.set('tarea', res);
+              })
+            } else {
+              this.storage.get('tarea_marcada').then(res => {
+                res.splice(index, 1);
+                this.tareas_marcadas = res;
+                this.storage.set('tarea_marcada', res);
+              })
+            }
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
 }
